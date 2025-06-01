e361fb7513c744163626c25cdbfb905ead517219
ZumaAkbarID <rahmatwahyumaakbar@gmail.com>
2025-06-01 14:37:42 +0700

    Auth Context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { createContext, type ReactNode } from "react";
+import Cookies from "js-cookie";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!Cookies.get("token")
+  );
+
+  useEffect(() => {
+    const handleTokenChange = () => {
+      setIsAuthenticated(!!Cookies.get("token"));
+    };
+
+    window.addEventListener("storage", handleTokenChange);
+
+    return () => {
+      window.removeEventListener("storage", handleTokenChange);
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./context/AuthContext";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
